8ad3089369e2285d0d017de2e5130c66a53602cb
Nicholas Ciechanowski <nciechanowski@staff.ventraip.com>
2020-12-14 14:49:50 +1100

    Some more work

--- src/components/Draggable.js
+++ src/components/Draggable.js
@@ -23,52 +23,58 @@ class Draggable extends Component {
       //props
     } = this.props;
     const {
-      //states
+      zLevel
     } = this.state;
     const {
       isClicked,
       // functions
     } = this;
 
-    return (
+    function dragWindow() {
+      return (
       <Rnd
-        onMouseDown={isClicked}
-        disableDragging={false}
-        dragHandleClassName={"handle"}
-        minHeight={"200px"}
+      onMouseDown={isClicked}
+      disableDragging={false}
+      dragHandleClassName={"handle"}
+      minHeight={"200px"}
+      style={{
+        boxShadow: "0 4px 16px rgba(0,0,0,0.2)",
+        zIndex: zLevel,
+      }}
+      default={{
+        x: 200,
+        y: 200,
+        width: 320,
+        height: 200,
+      }}
+    >
+      <div
+        className="handle"
         style={{
-          boxShadow: "0 4px 16px rgba(0,0,0,0.2)",
-          zIndex: this.state.zLevel,
-        }}
-        default={{
-          x: 200,
-          y: 200,
-          width: 320,
-          height: 200,
+          backgroundColor: "gray",
+          padding: "1rem",
         }}
       >
-        <div
-          className="handle"
-          style={{
-            backgroundColor: "gray",
-            padding: "1rem",
-          }}
-        >
-          Drag
-        </div>
-        <div
-          className="content"
-          style={{
-            width: "100%",
-            height: "calc( 100% - 3rem )",
-            borderWidth: "0",
-            backgroundColor: "white",
-          }}
-        >
-          {this.state.zLevel}
-          test
-        </div>
-      </Rnd>
+        Drag
+      </div>
+      <div
+        className="content"
+        style={{
+          width: "100%",
+          height: "calc( 100% - 3rem )",
+          borderWidth: "0",
+          backgroundColor: "white",
+        }}
+      >
+        {zLevel}
+        test
+      </div>
+    </Rnd>
+      )
+    }
+
+    return (
+      dragWindow()
     );
   }
 }
